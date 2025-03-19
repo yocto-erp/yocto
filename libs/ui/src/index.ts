70a40341cd6931bae7992cca983eed13ce0939d0
Le Phuoc Canh <lephuoccanh@gmail.com>
2025-03-19 12:45:08 +0700

    Add Card and YoctoTableCard

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -3,6 +3,7 @@ export * from "./lib/form/index";
 export * from "./lib/yo-table/models/index";
 export * from "./lib/ui";
 export * from "./lib/yo-table/yo-table";
+export * from "./lib/yo-table/yo-table-card";
 export * from "./lib/api/index";
 export * from "./lib/util/index";
 export * from "./lib/card/index"
